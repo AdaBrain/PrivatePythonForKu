b9e1cfaeeccaa6eb0e3eb18ae8a1a0ffa132f690
adadesions <adadesions@gmail.com>
2022-11-17 11:58:01 +0700

    Add practise problem

---
+++ day2/problem.py
@@ -0,0 +1,15 @@
+'''
+========================================================
+Given a dictionary, find mean of all the values present.
+========================================================
+Ex.1
+Input : 
+test_dict = {"Gfg" : 4, "is" : 4, "Best" : 4, "for" : 4, "Geeks" : 4} 
+Output : 4.0 
+Explanation : (4 + 4 + 4 + 4 + 4) / 4 = 4.0, hence mean. 
+========================================================
+Ex.2
+Input : test_dict = {"Gfg" : 5, "is" : 10, "Best" : 15} 
+Output : 10.0 
+Explanation : Mean of these is 10.0
+'''
